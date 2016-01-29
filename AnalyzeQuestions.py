efe87812176ca25bc63955eaa4b60b11a85bc44b
Ben Golden <scientist7@aol.com>
2016-01-28 22:07:57 -0700

    more work

--- AnalyzeQuestions.py
+++ AnalyzeQuestions.py
@@ -1,14 +1,30 @@
 import pickle
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Grab stuff from pickle jar
-file = open('data.pkl', 'rb')
+file = open('AllData.pkl', 'rb')
 
 Q_freq_table=pickle.load(file)
 Q_text_table=pickle.load(file)
+A_text_table=pickle.load(file)
 usernames=pickle.load(file)
 answeredquestions=pickle.load(file)
+myanswermatches=pickle.load(file)
+answers=pickle.load(file)
 
-for k in sorted(Q_freq_table, key=Q_freq_table.get, reverse=True)[:30]:
-    print Q_text_table[k], Q_freq_table[k]
+file.close()
+
+A_freq_table = {}
+
+entryIndex = 0
+subentryIndex = 0
+for aqs in answeredquestions:
+    for qid in aqs:
+    
+
+NQuestions = 1
+
+for k in sorted(Q_freq_table, key=Q_freq_table.get, reverse=True)[:NQuestions]:
+    N = len(A_text_table[k])
          
