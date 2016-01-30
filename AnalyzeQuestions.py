dcc6036fb5b8b91126ec3a819aab94c722c6c163
Ben Golden <scientist7@aol.com>
2016-01-29 23:45:58 -0700

    started histos

--- AnalyzeQuestions.py
+++ AnalyzeQuestions.py
@@ -17,14 +17,35 @@ file.close()
 
 A_freq_table = {}
 
+# Function to make hash table of answer id's
+def FillAnswer(qid,aid):
+    if qid in A_freq_table:
+        if aid in A_freq_table[qid]:
+            A_freq_table[qid][aid] += 1
+            
+        else:
+            A_freq_table[qid][aid] = 0  
+    else:
+        A_freq_table[qid] = {aid : 0}
+
+
 entryIndex = 0
 subentryIndex = 0
 for aqs in answeredquestions:
+    subentryIndex = 0
     for qid in aqs:
-    
+        aid = answers[entryIndex][subentryIndex]
+        FillAnswer(qid,aid)
+        subentryIndex += 1
+    entryIndex += 1    
 
 NQuestions = 1
 
 for k in sorted(Q_freq_table, key=Q_freq_table.get, reverse=True)[:NQuestions]:
     N = len(A_text_table[k])
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35       # the width of the bars
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind, A_freq_table[k], width, color='r')
+    plt.show()
          
