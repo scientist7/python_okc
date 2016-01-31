9a5d264199cbbaf43e79409ee57dfed42c7ea094
Ben Golden <scientist7@aol.com>
2016-01-30 22:37:25 -0700

    histos work!

--- AnalyzeQuestions.py
+++ AnalyzeQuestions.py
@@ -17,17 +17,17 @@ file.close()
 
 A_freq_table = {}
 
-# Function to make hash table of answer id's
-def FillAnswer(qid,aid):
-    if qid in A_freq_table:
-        if aid in A_freq_table[qid]:
-            A_freq_table[qid][aid] += 1
-            
+# Initialize A_freq_table
+keys = list(A_text_table.keys())
+for k in keys:
+    aids = list(A_text_table[k].keys())
+    count = 1
+    for aid in aids:
+        if count < 2: 
+            A_freq_table[k] = {aid : 0}
         else:
-            A_freq_table[qid][aid] = 0  
-    else:
-        A_freq_table[qid] = {aid : 0}
-
+            A_freq_table[k][aid] = 0
+        count += 1    
 
 entryIndex = 0
 subentryIndex = 0
@@ -35,17 +35,34 @@ for aqs in answeredquestions:
     subentryIndex = 0
     for qid in aqs:
         aid = answers[entryIndex][subentryIndex]
-        FillAnswer(qid,aid)
+        if aid < 0:
+            continue
+        A_freq_table[qid][aid] += 1
         subentryIndex += 1
     entryIndex += 1    
 
-NQuestions = 1
+NQuestions = 50
 
+qrank = 0
 for k in sorted(Q_freq_table, key=Q_freq_table.get, reverse=True)[:NQuestions]:
+    qrank += 1
+    print('Processing question ', qrank)
     N = len(A_text_table[k])
     ind = np.arange(N)  # the x locations for the groups
-    width = 0.35       # the width of the bars
+    width = 1       # the width of the bars
+    tally = list(A_freq_table[k].values())
+    sumtally = sum(tally)
+    if sumtally <= 0:
+        continue
+    normtally = [i*100/sumtally for i in tally]
     fig, ax = plt.subplots()
-    rects1 = ax.bar(ind, A_freq_table[k], width, color='r')
-    plt.show()
+    rects1 = ax.bar(ind, normtally, width, color='r')
+    # add some text for labels, title and axes ticks
+    ax.set_ylabel('% of respondents')
+    ax.set_title(Q_text_table[k])
+    ax.set_xticks(ind + 0.5*width)
+    answerText = list(A_text_table[k].values())
+    ax.set_xticklabels(answerText)
+    fig.savefig('Question'+str(qrank)+'.png')
+    plt.close(fig)
          
